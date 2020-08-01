848ac2897d2528c55437fd8471af40cfbdb2d958
Jim Eliot <benjaminjeliot@gmail.com>
2020-08-01 11:16:19 +0100

    Create Example.h

---
+++ src/Example.h
@@ -0,0 +1,14 @@
+// Copyright 2020 Jim Eliot
+
+#ifndef SRC_EXAMPLE_H_
+#define SRC_EXAMPLE_H_
+
+class Example {
+ public:
+  void SetValue(int value);
+  int GetValue();
+ private:
+  int value_;
+};
+
+#endif  // SRC_EXAMPLE_H_
